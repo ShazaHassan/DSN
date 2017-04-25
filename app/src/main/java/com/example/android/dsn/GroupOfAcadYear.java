4dbc9a1121ed52b4e69f4faf7992d453abe732fe
Shaza Hassan <shazahassan2020@gmail.com>
2017-04-25 17:50:48 +0200

    edit in page of select academic  year edit in page of group of academic add about page

--- app/src/main/java/com/example/android/dsn/GroupOfAcadYear.java
+++ app/src/main/java/com/example/android/dsn/GroupOfAcadYear.java
@@ -1,22 +1,47 @@
 package com.example.android.dsn;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupOfAcadYear extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+
+
+
+
+    LinearLayout addPost;
+    EditText writePost;
+    TextView post;
+    Button postButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_of_acad_year);
+
+
+
+
+
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(SelectAcadYear.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.acadYear);
+        textView.setText(message);
+
         // Spinner element
         Spinner spinner = (Spinner) findViewById(R.id.subjectSelect);
 
@@ -25,12 +50,12 @@ public class GroupOfAcadYear extends AppCompatActivity implements AdapterView.On
 
         // Spinner Drop down elements
         List<String> categories = new ArrayList<String>();
-        categories.add("Automobile");
-        categories.add("Business Services");
-        categories.add("Computers");
-        categories.add("Education");
-        categories.add("Personal");
-        categories.add("Travel");
+        categories.add("select subject");
+        categories.add("Software 1");
+        categories.add("Math");
+        categories.add("UI");
+        categories.add("Software 2");
+
 
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
@@ -41,8 +66,24 @@ public class GroupOfAcadYear extends AppCompatActivity implements AdapterView.On
         // attaching data adapter to spinner
         spinner.setAdapter(dataAdapter);
 
+
+
+        //when click on post button
+        addPost=(LinearLayout) findViewById(R.id.mainPage);
+        post =new TextView(this);
+        postButton=(Button) findViewById(R.id.post);
+        writePost= (EditText) findViewById(R.id.writePost) ;
+        postButton.setOnClickListener(onClick());
+
+
+
+
+
+        //setOnClickListener(onClick());
+
     }
 
+    //this show which item is selected
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
@@ -51,10 +92,39 @@ public class GroupOfAcadYear extends AppCompatActivity implements AdapterView.On
         // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
 
+        Intent registerIntent = new Intent(GroupOfAcadYear.this, AboutSubject.class);
+        startActivity(registerIntent);
+
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+
+
+
+    //when click on post button add post to page
+
+    private View.OnClickListener onClick() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                addPost.addView(addNewPost(writePost.getText().toString()));
+                writePost.setText("");
+            }
+        };
+    }
+
+
+    private TextView addNewPost(String text) {
+        final ActionBar.LayoutParams layoutParams= new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
+
+        post.setLayoutParams(layoutParams);
+        post.setText(text);
+        return post;
     }
 }
--- app/src/main/java/com/example/android/dsn/SelectAcadYear.java
+++ app/src/main/java/com/example/android/dsn/SelectAcadYear.java
@@ -5,43 +5,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import static android.R.id.message;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class SelectAcadYear extends AppCompatActivity {
+
+
+    protected Button first,second,third,forth;
+
+    public static final String EXTRA_MESSAGE = "com.example.android.dsn.MESSAGE";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_acad_year);
-        final Button first= (Button) findViewById(R.id.firstyear);
-        final Button second= (Button) findViewById(R.id.secondyear);
-        final Button third= (Button) findViewById(R.id.thirdyear);
-        final Button forth= (Button) findViewById(R.id.forthyear);
+
+        first=(Button) findViewById(R.id.firstyear);
+        second=(Button)  findViewById(R.id.secondyear);
+        third= (Button) findViewById(R.id.thirdyear);
+        forth=(Button) findViewById(R.id.forthyear);
+
+
+
+        // go to academic year page and write the name of year
         first.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(SelectAcadYear.this, GroupOfAcadYear.class);
-                SelectAcadYear.this.startActivity(registerIntent);
+                String fi= first.getText().toString();
+                registerIntent.putExtra(EXTRA_MESSAGE, fi);
+                startActivity(registerIntent);
             }
         });
         second.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(SelectAcadYear.this, GroupOfAcadYear.class);
-                SelectAcadYear.this.startActivity(registerIntent);
+                String fi= second.getText().toString();
+                registerIntent.putExtra(EXTRA_MESSAGE, fi);
+                startActivity(registerIntent);
             }
         });
         third.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(SelectAcadYear.this, GroupOfAcadYear.class);
-                SelectAcadYear.this.startActivity(registerIntent);
+                String fi= third.getText().toString();
+                registerIntent.putExtra(EXTRA_MESSAGE, fi);
+                startActivity(registerIntent);
             }
         });
         forth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(SelectAcadYear.this, GroupOfAcadYear.class);
-                SelectAcadYear.this.startActivity(registerIntent);
+                String fi= forth.getText().toString();
+                registerIntent.putExtra(EXTRA_MESSAGE, fi);
+                startActivity(registerIntent);
             }
         });
     }
